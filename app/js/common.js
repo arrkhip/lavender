e9aa174be1500651aeccf781bd9573d647709816
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-02-07 15:29:51 +0300

    created accordion

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
 
+// accordion
+$(function() {
+    var caption = $('.js-accordion__header'),
+        content = $('.accordion__body');   
+        icon    = $('.accordion__icon') 
+             
+    caption.click(function(e) {
+    		content.not($(this).next(content)).slideUp(600);
+        $(this).next(content).slideToggle(600);
+        icon.not($(this).find('.accordion__icon')).removeClass('accordion__icon--open');
+        $(this).find('.accordion__icon').toggleClass('accordion__icon--open');
+    });
+});
   
 });
