a40406e3a4d034b023d3c566009a0d8eba7955c6
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-02-12 06:11:26 +0300

    header scrool

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,45 @@
 $(document).ready(function () {
 
+// header
+
+$(document).on("scroll", onScroll);
+
+    $('a[href^="#"]').on('click', function (e) {
+    	e.preventDefault();
+    	$(document).off("scroll");
+
+    	$('a').each(function () {
+    		$(this).removeClass('header__nav-link--active');
+    	})
+    	$(this).addClass('header__nav-link--active');
+
+    	var target = this.hash,
+    	menu = target;
+    	$target = $(target);
+    	$('html, body').stop().animate({
+    		'scrollTop': $target.offset().top - 170
+    	}, 500, 'swing', function () {
+    		window.location.hash = target + 170;
+    		$(document).on("scroll", onScroll);
+    	});
+    });
+
+function onScroll(event){
+	var scrollPos = $(document).scrollTop();
+	$('.header__nav-link').each(function () {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+			$('.header__nav-link').removeClass("header__nav-link--active");
+			currLink.addClass("header__nav-link--active");
+		}
+		else{
+			currLink.removeClass("header__nav-link--active");
+		}
+	});
+}
+
+
 // accordion
 $(function() {
 	var caption = $('.js-accordion__header'),
